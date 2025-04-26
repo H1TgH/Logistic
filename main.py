13b29ece75085e1da9adbfaf80f74d343d4365d7
Ivan Fedorov <ivan45sc@gmail.com>
2025-04-26 17:18:59 +0500

    changed user model and create migration

--- main.py
+++ main.py
@@ -37,14 +37,49 @@ class Role(PyEnum):
 class UserModel(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True, index=True, nullable=False)
-    email: Mapped[str] = mapped_column(unique=True, nullable=False)
-    username: Mapped[str] = mapped_column(String(32),unique=True, nullable=False)
-    phone: Mapped[str] = mapped_column(String(12), nullable=False)
-    password: Mapped[str] = mapped_column(String(128), nullable=False)
-    role: Mapped[Role] = mapped_column(Enum(Role), default=Role.USER, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(nullable=False, server_default=func.now())
-    updated_at: Mapped[datetime] = mapped_column(nullable=False, onupdate=func.now())
+    id: Mapped[int] = mapped_column(
+        BigInteger, 
+        primary_key=True, 
+        autoincrement=True, 
+        index=True, 
+        nullable=False
+    )
+    email: Mapped[str] = mapped_column(
+        unique=True, 
+        nullable=False
+    )
+    username: Mapped[str] = mapped_column(
+        String(32), 
+        unique=True, 
+        nullable=False
+    )
+    phone: Mapped[str] = mapped_column(
+        String(20), 
+        unique=True,
+        nullable=False
+    )
+    password: Mapped[str] = mapped_column(
+        String(128), 
+        nullable=False
+    )
+    api_key: Mapped[str] = mapped_column(
+        String(64),
+        unique=True,
+        nullable=True
+    )
+    role: Mapped[Role] = mapped_column(
+        Enum(Role), 
+        default=Role.USER, 
+        nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        nullable=False, 
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        nullable=False,
+        onupdate=func.now()
+    )
 
 
 class UserRegistrationSchema(BaseModel):
