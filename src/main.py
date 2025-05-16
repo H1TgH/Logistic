80163dab376b31a20304e853c040405fb8bb0275
Ivan Fedorov <ivan45sc@gmail.com>
2025-05-16 22:35:04 +0500

    add CORS in main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.users.router import main_router
 from src.reviews.router import review_router
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(main_router)
 app.include_router(review_router)
